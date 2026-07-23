81568d9f65b8b2fcc0d43996af14a50f134d7628
someguythat-thinkswithportals <rozavkoa@gmail.com>
2026-07-23 11:48:16 +0300

    changed the number to 0.4.1 and not 0.2

--- userspace/uname.c
+++ userspace/uname.c
@@ -1,6 +1,6 @@
 #include <some-libc.h>
 
 int main(void) {
-    printf("some-tiny-os some-tiny-os 0.2 x86_64 some-tiny-os\n");
+    printf("some-tiny-os some-tiny-os 0.4.1 x86_64 some-tiny-os\n");
     return 0;
 }
